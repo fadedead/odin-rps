10668b586aafe80531b2455dd161a825ff7b716d
fadedead <fadedeadd@gmail.com>
2023-05-29 18:49:54 +0530

    Added: added the rounds implementation

--- rps.js
+++ rps.js
@@ -7,52 +7,53 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
   let win = -1;
+  let winner; 
   if (playerSelection === "rock") {
     if (computerSelection === "rock") {
-      console.log("It's a tie, both pick Rock");
+      winner ="It's a tie, both pick Rock";
       win = -1;
     }
     if (computerSelection === "paper") {
-      console.log("You Lose! Paper beats Rock");
+      winner ="You Lose! Paper beats Rock";
       win = 0;
     }
     if (computerSelection === "scissors") {
-      console.log("You Win! Rock beats Scissors");
+      winner ="You Win! Rock beats Scissors";
       win = 1;
     }
   }
 
   if (playerSelection === "paper") {
     if (computerSelection === "rock") {
-      console.log("You Win! Paper beats Rock");
+      winner ="You Win! Paper beats Rock";
       win = 1;
     }
     if (computerSelection === "paper") {
-      console.log("It's a tie, both pick Paper");
+      winner ="It's a tie, both pick Paper";
       win = -1;
     }
     if (computerSelection === "scissors") {
-      console.log("You Lose! Scissors beats Paper");
+      winner = "You Lose! Scissors beats Paper";
       win = 0;
     }
   }
 
   if (playerSelection === "scissors") {
     if (computerSelection === "rock") {
-      console.log("You Lose! Rock beats Scissors");
+      winner = "You Lose! Rock beats Scissors";
       win = 0;
     }
     if (computerSelection === "paper") {
-      console.log("You Win! Scissors beats Paper");
+      winner = "You Win! Scissors beats Paper";
       win = 1;
     }
     if (computerSelection === "scissors") {
-      console.log("It's a tie, both pick Scissors");
+      winner = "It's a tie, both pick Scissors";
       win = -1;
     }
   }
 
-  return win;
+  return [winner, win];
 }
 
 // function game() {
@@ -75,13 +76,13 @@ function playRound(playerSelection, computerSelection) {
 //     round += 1
 
 //   if(player > computer){
-//     console.log(`You Win! with score: ${player}`)
+//     winner = `You Win! with score: ${player}`)
 //   }
 //   else if(player < computer){
-//     console.log(`You Lose! with score: ${player}`)
+//     winner = `You Lose! with score: ${player}`)
 //   }
 //   else{
-//     console.log(`It's a Tie! with score: ${player}`)
+//     winner = `It's a Tie! with score: ${player}`)
 //   }
 // }
 
@@ -100,17 +101,20 @@ function createButtons() {
   const rock = document.createElement("button");
   const paper = document.createElement("button");
   const scissors = document.createElement("button");
+  const play = document.createElement("button");
 
   rock.innerText = "Play Rock";
   paper.innerText = "Play Paper";
   scissors.innerText = "Play Scissors";
+  play.innerText = "Play Again";
 
   const divRock = document.createElement("div");
   const divPaper = document.createElement("div");
   const divScissor = document.createElement("div");
+  const divPlay = document.createElement("div");
 
-  const btns = [rock, paper, scissors];
-  const divEle = [divRock, divPaper, divScissor];
+  const btns = [rock, paper, scissors, play];
+  const divEle = [divRock, divPaper, divScissor, divPlay];
 
   for (let i = 0; i < btns.length; i++) {
     divEle[i].appendChild(btns[i]);
@@ -125,7 +129,7 @@ function createButtons() {
     color: "grey",
     margin: "12px",
     padding: "12px",
-    paddingLeft: "48px",
+    paddingLeft: "0px",
     gap: "12px",
   };
 
@@ -135,67 +139,114 @@ function createButtons() {
 
   document.body.appendChild(div);
 
-  let rounds = document.createElement('div');
+  let rounds = document.createElement("div");
 
   const roundStyle = {
-    display : "flex",
-    flexDirection : "column",
-    fontSize : "12px",
-    margin : '0px',
-  }
+    display: "flex",
+    flexDirection: "column",
+    fontSize: "16px",
+    margin: "0px",
+    padding: "4px",
+  };
 
   playArea.appendChild(rounds);
   Object.assign(rounds.style, roundStyle);
-  
+
+ 
+  let roundCount = 0, currPlayerScore = 0, currComputerScore = 0;
+
   function playRock() {
-    const winner = playRound("rock", getComputerChoice());
-    let currWinner;
-    if (winner == 0) {
-      currWinner = "Computer Wins";
-    }
-    if (winner == 1) {
-      currWinner = "Player Wins";
-    }
-    if (winner == -1) {
-      currWinner = "It was a Tie";
-    }
-    let paraTag = document.createElement('p');
-    let currText = document.createTextNode(currWinner);
-    paraTag.style.margin = '0px';
-    rounds.appendChild(paraTag);
-    paraTag.appendChild(currText);
+    const winner = playRound("rock", getComputerChoice()); 
+    let end = btnUtil(winner);
+    if(end) clearRounds();
   }
 
   function playPaper() {
     const winner = playRound("paper", getComputerChoice());
-    if (winner == 0) {
-      alert("Computer Wins");
-    }
-    if (winner == 1) {
-      alert("Player Wins");
-    }
-    if (winner == -1) {
-      alert("It was a Tie");
+    let clear = btnUtil(winner);
+    if(clear) clearRounds();
+  }
+
+  function playScissors(){
+    const winner = playRound("scissors", getComputerChoice());
+    let clear = btnUtil(winner);
+    if(clear) clearRounds();
+  }
+
+  function playAgain(){
+    rock.onclick = playRock;
+    paper.onclick = playPaper;
+    scissors.onclick = playScissors;
+    let child = rounds.lastElementChild;
+    while(child){
+      rounds.removeChild(child);
+      child = rounds.lastElementChild;
     }
   }
 
-  function playScissors() {
-    const winner = playRound("scissors", getComputerChoice());
-    if (winner == 0) {
-      alert("Computer Wins");
+  function btnUtil(winner){
+    if(winner[1] == 0){
+      currComputerScore += 1;
+     }
+     if(winner[1] == 1){
+      currPlayerScore += 1;
     }
-    if (winner == 1) {
-      alert("Player Wins");
+     if(winner[1] == -1){
+      currPlayerScore += 1;
+      currComputerScore += 1;
+     }
+     roundCount += 1;
+     let paraTag = document.createElement("p");
+    let currText = document.createTextNode(`Round ${roundCount}: ${winner[0]}`);
+    paraTag.style.margin = "0px";
+    rounds.appendChild(paraTag);
+    paraTag.appendChild(currText);
+    if (roundCount == 5){
+      let winParaTag, currWin;
+      if(currPlayerScore > currComputerScore){
+        winParaTag = document.createElement("p");
+        currWin = document.createTextNode(`You Win! with score: ${currPlayerScore}`);
+        
+      }
+      else if(currPlayerScore < currComputerScore){
+        winParaTag = document.createElement("p");
+        currWin = document.createTextNode(`You Lose! with score: ${currPlayerScore}`);
+      }
+      else{
+        winParaTag = document.createElement("p");
+        currWin = document.createTextNode(`It's a Tie! with score: ${currPlayerScore}`);
+      }
+      winParaTag.style.margin = "0px";
+      rounds.appendChild(winParaTag);
+      winParaTag.appendChild(currWin);
+      return 1;
     }
-    if (winner == -1) {
-      alert("It was a Tie");
-    }
+    return 0;
   }
 
   rock.onclick = playRock;
   paper.onclick = playPaper;
   scissors.onclick = playScissors;
+  play.onclick = playAgain;
+
+  function clearRounds(){
+    roundCount = 0, currPlayerScore = 0, currComputerScore = 0;
+    rock.onclick = null;
+  paper.onclick = null;
+  scissors.onclick = null;
+  }
+
+  let bodyStyles = {
+    display : "flex",
+    flexDirection : "column",
+    alignItems : "center",
+    justifyContent : "center",
+  }
+  // console.log(document);
+  Object.assign(document.body.style, bodyStyles);
 }
+
+
 function main() {
   createButtons();
   // evenHandling(btns[0], btns[1], btns[2]);
